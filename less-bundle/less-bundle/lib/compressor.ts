93984d4ec4e0fed861d936a05662e43e716054cc
Cyprien Taque <cyprien.taque@gmail.com>
2020-04-25 22:01:37 +0200

    sync to async

--- less-bundle/less-bundle/lib/compressor.ts
+++ less-bundle/less-bundle/lib/compressor.ts
@@ -9,7 +9,7 @@ import generateOutput = require('./generateoutput');
 function writeToFile(path: string, data: Array<string>) {
     try {
         var fd = fs.openSync(path, 'w'),
-            buffer = new Buffer(data.join('\n'), 'utf8');
+        buffer = new Buffer(data.join('\n'), 'utf8');
 
         fs.writeSync(fd, buffer, 0, buffer.length, 0);
         fs.closeSync(fd);
@@ -26,16 +26,16 @@ function writeToFile(path: string, data: Array<string>) {
  * @param callback Since this is asynchronous, we need a callback to know 
  * when the task is complete.
  */
-function compress(config?: globals.IConfig, callback?: (err) => void) {
+async function compress(config?: globals.IConfig, callback?: (err) => void) {
     globals.initialize(config);
 
     var src = path.resolve(globals.config.src),
-        writers = globals.writers,
-        output = globals.output,
-        dest = globals.config.dest,
-        version = globals.config.version,
-        license = globals.config.license;
-    
+    writers = globals.writers,
+    output = globals.output,
+    dest = globals.config.dest,
+    version = globals.config.version,
+    license = globals.config.license;
+
     // Goes through each file in the dest files and makes sure they have a 
     // .less extension.
     dest.forEach((outFile, index) => {
@@ -45,7 +45,8 @@ function compress(config?: globals.IConfig, callback?: (err) => void) {
 
     // Reads the src file, builds the contents for each 
     // file in the proper order, and generates the output file.
-    fs.readFile(src, 'utf8', (err, data) => {
+    try{
+        const data = await fs.readFileSync(src, 'utf8')
         if (err) {
             return callback(err);
         }
@@ -60,9 +61,9 @@ function compress(config?: globals.IConfig, callback?: (err) => void) {
         // If a license file is specified, we want to prepend it to the output.
         if (!!license) {
             var licenseFile = path.resolve(license),
-                licenseData = fs.readFileSync(licenseFile, 'utf8'),
-                lines = licenseData.split(/\r\n|\n/),
-                regex = /(.*)v\d+\.\d+\.\d+(.*)/;
+            licenseData = fs.readFileSync(licenseFile, 'utf8'),
+            lines = licenseData.split(/\r\n|\n/),
+            regex = /(.*)v\d+\.\d+\.\d+(.*)/;
 
             // If a version is specified, we want to go through and find where 
             // the version is specified in the license, then replace it with the 
@@ -92,34 +93,10 @@ function compress(config?: globals.IConfig, callback?: (err) => void) {
         if (!!output[output.length - 1].trim()) {
             output.push('');
         }
-
-        // Go through each destination file and make sure we can 
-        // write a file to the location, making new directories 
-        // if necessary. Then write the output to each destination.
-        dest.forEach((destFile) => {
-            var destPath = path.resolve(destFile),
-                split = path.resolve(destPath, '..').split(/\/|\\/),
-                concat = '';
-
-            if (/[a-zA-Z]:/.test(split[0])) {
-                split.shift();
-                split.unshift('/' + split.shift());
-            }
-
-            split.forEach((val) => {
-                concat += val + '/';
-                try {
-                    fs.mkdirSync(concat);
-                } catch (err) {
-                    // The directory probably already exists.
-                }
-            });
-
-            writeToFile(destPath, output);
-        });
-
+        return output;
+    }catch(e){
         callback(err);
-    });
+    }
 }
 
 export = compress;
